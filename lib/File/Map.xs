Leont/file-map

commit a4c2c0fd69870a41789c00892828db5d10c0100e

Allocate memory for mmap_info as thread-shared

lib/File/Map.xs
@@ -239,7 +239,7 @@ static int mmap_free(pTHX_ SV* var, MAGIC* magic) {
 		MUTEX_DESTROY(&info->data_mutex);
 		MUTEX_UNLOCK(&info->count_mutex);
 		MUTEX_DESTROY(&info->count_mutex);
-		Safefree(info);
+		PerlMemShared_free(info);
 	}
 	else {
 		if (msync(info->real_address, info->real_length, MS_ASYNC) == -1)
@@ -249,7 +249,7 @@ static int mmap_free(pTHX_ SV* var, MAGIC* magic) {
 #else
 	if (munmap(info->real_address, info->real_length) == -1)
 		die_sys(aTHX_ "Could not unmap: %s");
-	Safefree(info);
+	PerlMemShared_free(info);
 #endif 
 	SvREADONLY_off(var);
 	SvPVX(var) = NULL;
@@ -336,8 +336,7 @@ static void* do_mapping(pTHX_ size_t length, int prot, int flags, int fd, off_t 
 }
 
 static struct mmap_info* initialize_mmap_info(void* address, size_t length, ptrdiff_t correction) {
-	struct mmap_info* magical;
-	New(0, magical, 1, struct mmap_info);
+	struct mmap_info* magical = PerlMemShared_malloc(sizeof *magical);
 	magical->real_address = address;
 	magical->fake_address = (char*)address + correction;
 	magical->real_length = length + correction;

commit e2d4492aab6c5f8df8508b71cc63053863fcebc1

Fixed deallocation bug introduced in a4c2c0fd69870
Fixed deallocation bug introduced in a4c2c0fd69870a41789c00892828db5d10c0100e

lib/File/Map.xs
@@ -266,13 +266,13 @@ static int empty_free(pTHX_ SV* var, MAGIC* magic) {
 		MUTEX_DESTROY(&info->data_mutex);
 		MUTEX_UNLOCK(&info->count_mutex);
 		MUTEX_DESTROY(&info->count_mutex);
-		Safefree(info);
+		PerlMemShared_free(info);
 	}
 	else {
 		MUTEX_UNLOCK(&info->count_mutex);
 	}
 #else
-	Safefree(info);
+	PerlMemShared_free(info);
 #endif 
 	SvREADONLY_off(var);
 	SvPV_free(var);